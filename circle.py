9c354ff959a72dcc0f67d761481dc15e5f420c1d
Fetisov Konstantin <145191088+k0ch3gar@users.noreply.github.com>
2023-12-20 13:33:14 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -9,20 +9,6 @@ class RectangleTestCase(unittest.TestCase):
     def test_square_area_mul(self):
         res = area(10)
         self.assertEqual(res, math.pi * 100)
-       
-    def test_float_area_mul(self):
-        self.assertEqual(area(2 / (math.pi ** 0.5)), float(4))
-        
-    def test_zero_per_mul(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)
-        
-    def test_square_per_mul(self):
-        res = perimeter(10)
-        self.assertEqual(res, math.pi * 20)
-       
-    def test_float_per_mul(self):
-        self.assertEqual(perimeter(2 / math.pi), float(4))
 
 
 
